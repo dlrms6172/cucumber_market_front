a23b088e88286f1e86f8fff953b27b08fc838f92
MoonHeonyoung <xiahvc@kau.ac.kr>
2024-07-09 23:02:34 +0900

    vue 폴더 구조 및 main.vue 생성

---
+++ cucumbervue/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>header</div>
+</template>
+<script>
+// import
+ export default {
+   name: '',
+   props: [],
+   components: {},
+   data() {
+     return {
+       sampleData: ''
+     };
+   },
+   setup() {}, // 컴포지션 API
+   created() {},
+   mounted() {},
+   unmounted() {},
+   methods: {},
+   computed: {},
+   watch: {}
+ }
+</script>
+<style>
+
+</style>
---
+++ cucumbervue/src/views/Main.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+<script>
+ import footer from '@/components/Footer.vue'
+ import header from '@/components/Header.vue'
+
+ export default {
+   name: '',
+   props: [],
+   components: {},
+   data() {
+     return {
+       sampleData: ''
+     };
+   },
+   setup() {}, // 컴포지션 API
+   created() {},
+   mounted() {},
+   unmounted() {},
+   methods: {},
+   computed: {},
+   watch: {}
+ }
+</script>
+<style>
+
+</style>
