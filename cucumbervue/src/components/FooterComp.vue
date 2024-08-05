92b0cc876a146e79f5493051da746000b4506718
MoonHeonyoung <xiahvc@kau.ac.kr>
2024-08-06 02:07:40 +0900

    Update FooterComp.vue

--- cucumbervue/src/components/FooterComp.vue
+++ cucumbervue/src/components/FooterComp.vue
@@ -1,118 +1,104 @@
 <template>
   <div id="footer-root">
     <div class="light-theme">
-      <footer class="_1trxeqs0">
-        <div class="_1trxeqs1 _1y9kelh2">
-          <section class="_1trxeqsi">
-            <section class="_1trxeqs2">
-              <div class="_1trxeqs3">당근 앱 다운로드</div>
-              <div class="_1trxeqs7"><a class="_1trxeqs4 _1trxeqsp" target="_blank"
-                                        href="https://daangn.onelink.me/oWdR/duyc3blt?afdp=karrot://open?from=website&amp;deep_link_value=karrot://open?from=website">
-                <div class="_1trxeqs5">iOS</div>
-                <span class="_1trxeqs6"><img src=""></span>
-                <div class="_1trxeqs8">
-                  <img src="">
-                </div>
-              </a><a class="_1trxeqs4 _1trxeqsp" target="_blank"
-                     href="https://daangn.onelink.me/oWdR/duyc3blt?afdp=karrot://open?from=website&amp;deep_link_value=karrot://open?from=website">
-                <div class="_1trxeqs5">Android</div>
-                <span class="_1trxeqs6"><img src=""></span>
-                <div class="_1trxeqs8">
-                  <img src="">
-                </div>
-              </a></div>
+      <footer class="footer-container">
+        <div class="footer-content">
+          <section class="app-download-section">
+            <section class="download-links">
+              <div class="download-title">당근 앱 다운로드</div>
+              <div class="download-icons">
+                <a class="download-link" target="_blank" href="https://daangn.onelink.me/oWdR/duyc3blt?afdp=karrot://open?from=website&deep_link_value=karrot://open?from=website">
+                  <div class="icon-container">
+                    <img src="/images/main/13.svg">
+                  </div>
+                </a>
+                <a class="download-link" target="_blank" href="https://daangn.onelink.me/oWdR/duyc3blt?afdp=karrot://open?from=website&deep_link_value=karrot://open?from=website">
+                  <div class="icon-container">
+                    <img src="/images/main/13.svg">
+                  </div>
+                </a>
+              </div>
             </section>
-            <nav aria-label="footer" class="_1trxeqs9">
-              <ul class="_1trxeqsa">
-                <li class="_1trxeqsq"><a class="undefined _1trxeqsp" target="_blank"
-                                         href="https://www.daangn.com/fleamarket/">중고거래</a></li>
-                <li class="_1trxeqsq"><a class="undefined _1trxeqsp" target="_blank"
-                                         href="https://www.daangn.com/kr/nearby-stores/">동네업체</a></li>
-                <li class="_1trxeqsq"><a class="undefined _1trxeqsp" target="_blank"
-                                         href="https://www.daangn.com/kr/jobs/">당근알바</a></li>
-                <li class="_1trxeqsq"><a class="undefined _1trxeqsp" target="_blank"
-                                         href="https://www.daangn.com/kr/realty/">부동산</a></li>
-                <li class="_1trxeqsq"><a class="undefined _1trxeqsp" target="_blank"
-                                         href="https://www.daangn.com/kr/car/">중고차 직거래</a></li>
+            <nav aria-label="footer" class="footer-nav">
+              <ul class="nav-links">
+                <li class="nav-item"><a class="link" target="_blank" href="https://www.daangn.com/fleamarket/">중고거래</a></li>
+                <li class="nav-item"><a class="link" target="_blank" href="https://www.daangn.com/kr/nearby-stores/">동네업체</a></li>
+                <li class="nav-item"><a class="link" target="_blank" href="https://www.daangn.com/kr/jobs/">당근알바</a></li>
+                <li class="nav-item"><a class="link" target="_blank" href="https://www.daangn.com/kr/realty/">부동산</a></li>
+                <li class="nav-item"><a class="link" target="_blank" href="https://www.daangn.com/kr/car/">중고차 직거래</a></li>
               </ul>
-              <ul class="_1trxeqsa">
-                <li class="_1trxeqsq"><a class="undefined _1trxeqsp" target="_blank"
-                                         href="https://business.daangn.com/">당근비즈니스</a></li>
-                <li class="_1trxeqsq"><a class="undefined _1trxeqsp" target="_blank"
-                                         href="https://chat.daangn.com/onboarding">채팅하기</a></li>
+              <ul class="nav-links">
+                <li class="nav-item"><a class="link" target="_blank" href="https://business.daangn.com/">당근비즈니스</a></li>
+                <li class="nav-item"><a class="link" target="_blank" href="https://chat.daangn.com/onboarding">채팅하기</a></li>
               </ul>
-              <ul class="_1trxeqsa">
-                <li class="_1trxeqsq"><a class="undefined _1trxeqsp" target="_blank"
-                                         href="https://cs.kr.karrotmarket.com/wv/faqs">자주 묻는 질문</a></li>
-                <li class="_1trxeqsq"><router-link to="/about">회사소개</router-link></li>
-                <li class="_1trxeqsq"><a class="undefined _1trxeqsp" target="_blank"
-                                         href="https://about.daangn.com/jobs">인재 채용</a></li>
+              <ul class="nav-links">
+                <li class="nav-item"><a class="link" target="_blank" href="https://cs.kr.karrotmarket.com/wv/faqs">자주 묻는 질문</a></li>
+                <li class="nav-item"><router-link to="/about">회사소개</router-link></li>
+                <li class="nav-item"><a class="link" target="_blank" href="https://about.daangn.com/jobs">인재 채용</a></li>
               </ul>
             </nav>
           </section>
-          <section class="_1trxeqsj">
-            <section class="_1trxeqsb">
-              <section class="_1trxeqsc">
-                <div class="_1s38h9c0"><span><b>대표</b> <!-- -->김용현, 황도연</span><span class="_1trxeqsd">
-                  <img src="">
-                </span>
+          <section class="footer-info-section">
+            <section class="info-block">
+              <section class="info-details">
+                <div class="info-header">
+                  <span><b>대표</b> <!-- -->김용현, 황도연</span>
+                  <span class="info-space">
+                    <img src="">
+                  </span>
                   <span>
                     <b>사업자번호</b>
-                    <!-- -->375-87-00088</span></div>
+                    <!-- -->375-87-00088</span>
+                </div>
                 <div><b>직업정보제공사업 신고번호</b> <!-- -->J1200020200016</div>
                 <div><b>통신판매업 신고번호</b> <!-- -->2021-서울서초-2875</div>
-                <address class="_1trxeqse">
+                <address class="info-address">
                   <div><b>주소</b> <!-- -->서울특별시 구로구 디지털로 300, 10층 (당근서비스)</div>
-                  <div><span><b>전화</b> <!-- -->1544-9796</span><span class="_1trxeqsd">
+                  <div><span><b>전화</b> <!-- -->1544-9796</span><span class="info-space">
                     <img src="">
                   </span><span><b>고객문의</b> <!-- -->cs@daangnservice.com</span></div>
                 </address>
               </section>
-              <section class="_1trxeqsk"><a class="_1trxeqsg _1trxeqsp" target="_blank"
-                                            href="mailto:contact@daangn.com">제휴 문의</a><a class="_1trxeqsg _1trxeqsp"
-                                                                                         target="_blank"
-                                                                                         href="mailto:ad@daangn.com">광고
-                문의</a><a class="_1trxeqsg _1trxeqsp" target="_blank" href="mailto:pr@daangn.com">PR 문의</a><a
-                  class="_1trxeqsg _1trxeqsp" target="_blank" href="mailto:ir@daangn.com">IR 문의</a></section>
+              <section class="info-contact-links">
+                <a class="contact-link" target="_blank" href="mailto:contact@daangn.com">제휴 문의</a>
+                <a class="contact-link" target="_blank" href="mailto:ad@daangn.com">광고 문의</a>
+                <a class="contact-link" target="_blank" href="mailto:pr@daangn.com">PR 문의</a>
+                <a class="contact-link" target="_blank" href="mailto:ir@daangn.com">IR 문의</a>
+              </section>
             </section>
-            <section class="_1trxeqsl"><a class="_1trxeqsm _1trxeqsp" target="_blank" aria-label="facebook"
-                                          href="https://www.facebook.com/daangn">
-              <img src="">
-            </a><a class="_1trxeqsm _1trxeqsp" target="_blank" aria-label="instagram"
-                   href="https://www.instagram.com/daangnmarket/">
-              <img src="">
-            </a><a class="_1trxeqsm _1trxeqsp" target="_blank" aria-label="youtube"
-                   href="https://www.youtube.com/channel/UC8tsBsQBuF7QybxgLmStihA">
-              <img src="">
-            </a><a class="_1trxeqsm _1trxeqsp" target="_blank" aria-label="blog" href="https://blog.naver.com/daangn">
-              <img src="">
-            </a>
-              <div><label class="_1trxeqsn" for="label-select-nation">
-                <img src="">
-                <select id="label-select-nation" class="_1trxeqso">
-                  <option value="kr" selected="">한국</option>
-                  <option value="https://uk.karrotmarket.com">영국</option>
-                  <option value="https://ca.karrotmarket.com">캐나다</option>
-                  <option value="https://us.karrotmarket.com">미국</option>
-                  <option value="https://jp.karrotmarket.com">일본</option>
-                </select>
-                <img src="">
-              </label></div>
-            </section>
-            <section class="_1trxeqsf">
-              <div class="_1trxeqsg"><a class="_1trxeqsg _1trxeqsp" target="_blank"
-                                        href="https://www.daangn.com/policy/terms">이용약관</a></div>
-              <div class="_1trxeqsg _1trxeqsh"><a class="_1trxeqsg _1trxeqsh _1trxeqsp" target="_blank"
-                                                  href="https://privacy.daangn.com/">개인정보처리방침</a></div>
-              <div class="_1trxeqsg"><a class="_1trxeqsg _1trxeqsp" target="_blank"
-                                        href="https://cs.kr.karrotmarket.com/wv/faqs/slug/operation_policy">운영정책</a>
+            <section class="social-media-section">
+              <a class="social-media-link" target="_blank" aria-label="facebook" href="https://www.facebook.com/daangn">
+                <img src="/images/main/7.svg">
+              </a>
+              <a class="social-media-link" target="_blank" aria-label="instagram" href="https://www.instagram.com/daangnmarket/">
+                <img src="/images/main/8.svg">
+              </a>
+              <a class="social-media-link" target="_blank" aria-label="youtube" href="https://www.youtube.com/channel/UC8tsBsQBuF7QybxgLmStihA">
+                <img src="/images/main/youtube.svg">
+              </a>
+              <a class="social-media-link" target="_blank" aria-label="blog" href="https://blog.naver.com/daangn">
+                <img src="/images/main/blog.svg">
+              </a>
+              <div>
+                <label class="select-nation-label" for="label-select-nation">
+                  <img src="/images/main/3.svg">
+                  <select id="label-select-nation" class="select-nation">
+                    <option value="kr" selected="">한국</option>
+                    <option value="https://uk.karrotmarket.com">영국</option>
+                    <option value="https://ca.karrotmarket.com">캐나다</option>
+                    <option value="https://us.karrotmarket.com">미국</option>
+                    <option value="https://jp.karrotmarket.com">일본</option>
+                  </select>
+                </label>
               </div>
-              <div class="_1trxeqsg"><a class="_1trxeqsg _1trxeqsp" target="_blank"
-                                        href="https://www.daangn.com/policy/location">위치기반서비스 이용약관</a></div>
-              <div class="_1trxeqsg"><a class="_1trxeqsg _1trxeqsp" target="_blank"
-                                        href="https://www.daangn.com/wv/faqs/3994">이용자보호 비전과 계획</a></div>
-              <div class="_1trxeqsg"><a class="_1trxeqsg _1trxeqsp" target="_blank"
-                                        href="https://www.daangn.com/wv/faqs/9010">청소년보호정책</a></div>
+            </section>
+            <section class="policy-links-section">
+              <div class="policy-link"><a class="link" target="_blank" href="https://www.daangn.com/policy/terms">이용약관</a></div>
+              <div class="policy-link"><a class="link" target="_blank" href="https://privacy.daangn.com/">개인정보처리방침</a></div>
+              <div class="policy-link"><a class="link" target="_blank" href="https://cs.kr.karrotmarket.com/wv/faqs/slug/operation_policy">운영정책</a></div>
+              <div class="policy-link"><a class="link" target="_blank" href="https://www.daangn.com/policy/location">위치기반서비스 이용약관</a></div>
+              <div class="policy-link"><a class="link" target="_blank" href="https://www.daangn.com/wv/faqs/3994">이용자보호 비전과 계획</a></div>
+              <div class="policy-link"><a class="link" target="_blank" href="https://www.daangn.com/wv/faqs/9010">청소년보호정책</a></div>
             </section>
           </section>
         </div>
@@ -121,104 +107,186 @@
   </div>
 </template>
 
+
 <script setup>
 
 </script>
 
 <style scoped>
-#footer-root ._1trxeqs0 {
+div {
+  display: block;
+}
+* {
+  padding: 0;
+  margin: 0;
+  outline: none;
+  position: relative;
+  color: #212529;
+  font-weight: 400;
+}
+#footer-root .footer-container {
   padding: 0 1.6rem;
   box-sizing: border-box;
   width: 100%;
   max-width: 100vw;
 }
-
-#footer-root ._1trxeqs9 {
-  line-height: 1.5;
-  font-size: 1.3008rem;
-  display: flex;
-  max-width: 76.8rem;
-}
-
-#footer-root ._1trxeqsi {
-  padding: 4.8rem 0;
-}
-
 section, article, aside, footer, header, nav, hgroup {
   display: block;
 }
-
-#footer-root ._1trxeqs1 {
+#footer-root .footer-content {
   display: flex;
   flex-direction: column;
 }
-
+#footer-root .footer-content {
+  max-width: 76.8rem;
+  margin: 0 auto;
+}
+#footer-root .app-download-section {
+  padding: 4.8rem 0;
+}
 @media screen and (min-width: 768px) {
-  #footer-root ._1trxeqsj {
-    display: grid;
-    justify-content: space-between;
-    align-items: flex-start;
+  #footer-root .download-links {
+    float: right;
+    margin-bottom: 0;
+    display: flex;
+    flex-direction: column;
   }
 }
-
-#footer-root ._1trxeqsb {
-  grid-column: 1 / 3;
-  grid-row: 1 / 2;
-}
-
 @media screen and (min-width: 768px) {
-  #footer-root ._1trxeqsl {
-    margin-top: 0;
-    grid-column: 3 / 4;
-    grid-row: 1 / 2;
-  }
-}
-
-#footer-root ._1trxeqsf {
-  flex-wrap: wrap;
-  display: flex;
-  grid-column: 1 / 4;
-}
-
-@media screen and (min-width: 768px) {
-  #footer-root ._1trxeqs9 {
+  #footer-root .download-title {
     line-height: 1.4;
     font-size: 1.4rem;
   }
 }
-
-#footer-root ._1trxeqsq {
-  width: fit-content;
-  list-style: none;
+#footer-root .download-title {
+  font-weight: 700;
 }
-
-ul li {
-  margin: 1.6rem;
+#footer-root .download-icons {
+  display: flex;
 }
-
-ul {
-  list-style-type: disc;
+@media screen and (min-width: 768px) {
+  #footer-root .download-link {
+    margin-top: 0.8rem;
+    margin-left: 0;
+  }
 }
-
-#footer-root ._1trxeqsa {
+#footer-root .link {
+  text-decoration: none;
+  outline: 0;
+}
+#footer-root .download-link {
+  display: flex;
+}
+@media screen and (min-width: 768px) {
+  #footer-root .download-link:not(:first-child) {
+    margin-left: 1rem;
+  }
+}
+@media screen and (min-width: 768px) {
+  #footer-root .footer-nav {
+    line-height: 1.4;
+    font-size: 1.4rem;
+  }
+}
+#footer-root .footer-nav {
+  display: flex;
+  max-width: 76.8rem;
+}
+#footer-root .nav-links {
   display: flex;
   flex-direction: column;
   padding-inline-start: 0;
   margin-block-start: 0;
   margin-block-end: 0;
   width: 33%;
+  list-style-type: none;
 }
-
-#footer-root ._1trxeqsa ._1trxeqsq:not(:last-child) {
-  margin-bottom: 1.6rem;
+ul {
+  list-style-type: disc;
 }
-
-#footer-root ._1trxeqsa:last-child {
+#footer-root .nav-links:last-child {
   margin-right: 15%;
 }
-#footer-root ._1trxeqsj {
-  display: grid;
+@media screen and (min-width: 768px) {
+  #footer-root .footer-info-section {
+    display: grid;
+    justify-content: space-between;
+    align-items: flex-start;
+  }
+}
+#footer-root .footer-info-section {
   padding: 2.4rem 0 10rem;
-  border-top: 1px solid;
+  border-top: 1px solid
+}
+#footer-root .info-block {
+  grid-column: 1 / 3;
+  grid-row: 1 / 2;
+}
+@media screen and (min-width: 768px) {
+  #footer-root .info-details, #footer-root .contact-link {
+    line-height: 1.5;
+    font-size: 1.3008rem;
+  }
+}
+#footer-root .info-details {
+  margin-bottom: 1.6rem;
+}
+#footer-root .info-header {
+  display: flex;
+  align-items: center;
+}
+strong, b {
+  font-weight: 700;
+}
+#footer-root .info-space {
+  margin: 0 0.4rem;
+}
+#footer-root .info-address {
+  font-style: normal;
+}
+#footer-root .info-contact-links {
+  margin-bottom: 1.6rem;
+}
+#footer-root .contact-link:not(:last-child) {
+  margin-right: 1.6rem;
+}
+#footer-root .contact-link {
+  font-weight: 700;
+  white-space: nowrap;
+}
+@media screen and (min-width: 768px) {
+  #footer-root .social-media-section {
+    margin-top: 0;
+    grid-column: 3 / 4;
+    grid-row: 1 / 2;
+  }
+}
+#footer-root .social-media-section {
+  display: flex;
+  align-items: center;
+}
+#footer-root .social-media-section> .link:not(:last-child) {
+  margin-right: 1.6rem;
+}
+#footer-root .social-media-link {
+  padding-top: 0.4rem;
+}
+#footer-root .select-nation-label {
+  display: flex;
+  align-items: center;
+  padding: 0.8rem;
+  border-radius: 0.4rem;
+  cursor: pointer;
+}
+#footer-root .select-nation {
+  padding-left: 0.4rem;
+  border: none;
+  text-indent: 0.1rem;
+  cursor: pointer;
+}
+#footer-root .policy-links-section {
+  flex-wrap: wrap;
+  display: flex;
+  grid-column: 1 / 4;
 }
 </style>
